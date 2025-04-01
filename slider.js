99a71952b4eba2eb3749ae5f86bb579362ceeda9
mawzeee <mawzeeee@gmail.com>
2025-04-01 02:19:32 +0200

    Update slider.js

--- slider.js
+++ slider.js
@@ -137,7 +137,6 @@ class DepthMapEffect {
         x: 10,
         scaleX: 1.05,
         opacity: 0.8,
-        filter: 'hue-rotate(60deg) contrast(300%) saturate(2)',
         ease: 'none'
     })
     .to(this.graffitiImg, {
@@ -145,7 +144,6 @@ class DepthMapEffect {
         x: -15,
         scaleX: 0.95,
         opacity: 0.5,
-        filter: 'hue-rotate(-40deg) contrast(400%) brightness(200%)',
         ease: 'none'
     })
     .to(this.graffitiImg, {
@@ -153,16 +151,9 @@ class DepthMapEffect {
         x: 5,
         scaleX: 1,
         opacity: 0.2,
-        filter: 'saturate(3) contrast(150%)',
         ease: 'none'
     })
-    .to(this.graffitiImg, {
-        duration: 0.15,
-        x: 0,
-        opacity: 0,
-        filter: 'none',
-        ease: 'power2.out'
-    });
+    
 }
 
 
